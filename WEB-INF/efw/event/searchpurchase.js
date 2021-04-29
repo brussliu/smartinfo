b55e94a4c5cfdfdd4780eb119cd32f06a06d957b
brussliu <bruss.ji.liu@gmail.com>
2021-04-29 16:24:32 +0900

    fff
    ffff

--- WEB-INF/efw/event/searchpurchase.js
+++ WEB-INF/efw/event/searchpurchase.js
@@ -9,25 +9,25 @@ searchpurchase.fire=function(params){
 	var ret = new Result();
 
 	var resultHTML = 
-	"<tr style='height: 28px;'>"
-		"<td rowspan='2'>{purchase_no}</td>"
-		"<td >{purchase_name}</td>"
-		"<td >{quantity}</td>"
-		"<td >{amount}</td>"
-		"<td >{status}</td>"
-		"<td >{registration_date}</td>"
-		"<td >{confirm_date}</td>"
-		"<td >{shipping_date}</td>"
-		"<td >{arrival_date}</td>"
-		"<td >{acceptance_date}</td>"
-	"</tr>"
-	"<tr style='height: 28px;'>"
-		"<td colspan='4'><input type='file' style='width: 400px;'></td>"
-		"<td ><input type=button value='更　　新' style='width:80px;' onclick=''></td>"
-		"<td ><input type=button value='仕入確定' style='width:80px;' onclick=''></td>"
-		"<td ><input type=button value='発送した' style='width:80px;' onclick=''></td>"
-		"<td ><input type=button value='到着した' style='width:80px;' onclick=''></td>"
-		"<td ><input type=button value='受け取り' style='width:80px;' onclick=''></td>"
+	"<tr style='height: 28px;'>" +
+		"<td rowspan='2'>{purchase_no}</td>" +
+		"<td >{purchase_name}</td>" +
+		"<td >{quantity}</td>" +
+		"<td >{amount}</td>" +
+		"<td >{status}</td>" +
+		"<td >{registration_date}</td>" +
+		"<td >{confirm_date}</td>" +
+		"<td >{shipping_date}</td>" +
+		"<td >{arrival_date}</td>" +
+		"<td >{acceptance_date}</td>" +
+	"</tr>" +
+	"<tr style='height: 28px;'>" +
+		"<td colspan='4'><input type='file' style='width: 400px;'></td>" +
+		"<td ><input type=button value='更　　新' style='width:80px;' onclick=''></td>" +
+		"<td ><input type=button value='仕入確定' style='width:80px;' onclick=''></td>" +
+		"<td ><input type=button value='発送した' style='width:80px;' onclick=''></td>" +
+		"<td ><input type=button value='到着した' style='width:80px;' onclick=''></td>" +
+		"<td ><input type=button value='受け取り' style='width:80px;' onclick=''></td>" +
 	"</tr>";
 
 	var selectResult = db.select(
