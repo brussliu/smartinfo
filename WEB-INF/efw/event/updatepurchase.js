2f2a897d110d5fc46f4c9b68a6889d2830dd9feb
brussliu <bruss.ji.liu@gmail.com>
2021-04-30 08:02:41 +0900

    gg
    gg

--- WEB-INF/efw/event/updatepurchase.js
+++ WEB-INF/efw/event/updatepurchase.js
@@ -2,7 +2,7 @@ var updatepurchase={};
 updatepurchase.name="仕入新規登録";
 updatepurchase.paramsFormat={
 
-	"purchasefile" : "required:true;display-name:仕入内容;",
+	"#purchasefile" : "required:true;display-name:仕入内容;",
 	"#shop" : null
 };
 
@@ -12,7 +12,7 @@ updatepurchase.fire=function(params){
 
 	shopname = params["#shop"];
 
-	params["purchasefile"].debug("FFFFFFFFFFFFFFFFFF");
+	params["#purchasefile"].debug("FFFFFFFFFFFFFFFFFF");
 
 	// // 仕入名称
 	// var purchasename = params["#purchaseInfo"]["#purchasename"];
