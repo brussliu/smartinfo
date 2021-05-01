0aa0cc6542ba4346f8a223ab892be0224dd3cccc
brussliu <bruss.ji.liu@gmail.com>
2021-05-01 16:00:17 +0900

    wrerw
    dsf

--- WEB-INF/efw/event/updatepurchase.js
+++ WEB-INF/efw/event/updatepurchase.js
@@ -16,13 +16,13 @@ updatepurchase.fire=function(params){
 
 	// 仕入No
 	var purchaseno = params["#purchaseno"];
+
 	// 仕入名称
 	var purchasename = params["#purchasename"];
-
-	// 仕入管理テーブル登録
+	// 仕入名称更新
 	var updateResult = db.change(
 		"PURCHASE",
-		"updatePurchase0",
+		"updatePurchase00",
 		{
 			"shop":shopname,
 			"col0":purchaseno,
@@ -30,28 +30,115 @@ updatepurchase.fire=function(params){
 		}
 	);
 
-	// 仕入内容
+	// 仕入明細
 	var importfile_purchase = params["#importfile_purchase"];
 
 	if(importfile_purchase == null || importfile_purchase.length == 0){
 		return (new Result()).eval("Efw('menu_goto',{page:'si_purchase.jsp',shop:'"+ shopname + "'})");
 	}
 
+	// ステータス検索
+	var detailResult = db.select(
+		"PURCHASE",
+		"selectPurchaseStatus",
+		{
+			col0:purchaseno,
+			shop:shopname
+		}
+	).getArray();
+
+	var status = detailResult[0]["status"];
+
+	// 受取済みの場合、明細は更新しない
+	if(status == "4：受取済み"){
+		return (new Result()).eval("Efw('menu_goto',{page:'si_purchase.jsp',shop:'"+ shopname + "'})");
+
+	}
+	// 
+	if(status == "1：仕入確定"){
+
+			// 2, 既存仕入明細を全件削除
+			var delResult = db.change(
+				"PURCHASE",
+				"delPurchaseDetail",
+				{
+					"col0":purchaseno
+				}
+			);
+	
+			// 3, 新しい仕入明細を挿入
+			uploadPurchaseDetail(importfile_purchase);
+
+			// 5, 確定数量及び確定金額を再計算
+			var updateResult = db.change(
+				"PURCHASE",
+				"updatePurchase01",
+				{
+					col0:purchaseno,
+					shop:shopname
+				}
+			);
+
+	}
+
+	// 発送済みまたは到着済みの場合、途中在庫へ計上する処理を行う
+	if(status == "2：発送済み" || status == "3：到着済み"){
+
+		// 1, 途中在庫から既存仕入明細の数量を減らす
+		var updateResult = db.change(
+			"PURCHASE",
+			"updatePurchaseShipping1",
+			{
+				"col0":purchaseno
+			}
+		);
+
+		// 2, 既存仕入明細を全件削除
+		var delResult = db.change(
+			"PURCHASE",
+			"delPurchaseDetail",
+			{
+				"col0":purchaseno
+			}
+		);
+
+		// 3, 新しい仕入明細を挿入
+		uploadPurchaseDetail(importfile_purchase);
+
+		// 4, 途中在庫に新しい仕入明細の数量を計上
+		var updateResult = db.change(
+			"PURCHASE",
+			"updatePurchaseShipping2",
+			{
+				"col0":purchaseno
+			}
+		);
+
+		// 5, 確定数量及び確定金額を再計算
+		var updateResult = db.change(
+			"PURCHASE",
+			"updatePurchase01",
+			{
+				col0:purchaseno,
+				shop:shopname
+			}
+		);
+
+	}
+
+	return (new Result()).eval("Efw('menu_goto',{page:'si_purchase.jsp',shop:'"+ shopname + "'})");
+};
+
+
+function uploadPurchaseDetail(excelfile){
+
 	file.saveUploadFiles("upload");
 
-	var fa = importfile_purchase.split("\\");
+	var fa = excelfile.split("\\");
 	var f = fa[fa.length-1];
 
 	// Excelファイル
 	var excelXSSF = new Excel("upload/" + f);
-
-	var delResult = db.change(
-		"PURCHASE",
-		"delPurchaseDetail",
-		{
-			"col0":purchaseno
-		}
-	);
 
 	if(shopname == "Smart-KM"){
 
@@ -372,5 +459,22 @@ updatepurchase.fire=function(params){
 
 	}
 
-	return (new Result()).eval("Efw('menu_goto',{page:'si_purchase.jsp',shop:'"+ shopname + "'})");
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
