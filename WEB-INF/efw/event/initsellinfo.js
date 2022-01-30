35dad6a2c806a957856807a81d76dac9bf400442
brussliu <bruss.ji.liu@gmail.com>
2022-01-30 14:08:10 +0900

    fff
    gggg

---
+++ WEB-INF/efw/event/initsellinfo.js
@@ -0,0 +1,30 @@
+var initsellinfo={};
+initsellinfo.name="販売情報初期化";
+initsellinfo.paramsFormat={
+	"#shop":null
+};
+var shopname = "";
+initsellinfo.fire=function(params){
+	
+	var ret = new Result();
+	shopname = params["#shop"];
+	
+	var resultHTML = "<option id='W001'>{dt}</option>";
+
+	var selectResult = db.select(
+		"SELLINFO",
+		"selectDt",
+		{shop:shopname}
+	).getArray();
+
+	//ret.runat("#datefrom").remove("option").append(resultHTML).withdata(selectResult);
+
+	//ret.runat("#dateto").remove("option").append(resultHTML).withdata(selectResult);
+
+	// var script = "$('#productlist').show();";
+	// ret.eval(script);
+
+	// 画面へ結果を返す
+	return ret;
+
+};
