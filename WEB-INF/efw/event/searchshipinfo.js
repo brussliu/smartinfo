fbe7c69c8db94dac45def023501cb50637478e24
brussliu <38742004+brussliu@users.noreply.github.com>
2022-05-08 19:21:39 +0900

    sss
    sss

--- WEB-INF/efw/event/searchshipinfo.js
+++ WEB-INF/efw/event/searchshipinfo.js
@@ -11,16 +11,18 @@ searchshipinfo.fire=function(params){
 
 	var resultHTML = 
 	"<tr class='content' style=''>" +
-		"<td style='width: 50px'><input type='radio' name='selectshipno' value='{shipno}' onclick='activebutton();'></input></td>" +
+		"<td style='width: 40px'><input type='radio' name='selectshipno' value='{shipno}' onclick='activebutton();'></input></td>" +
 		"<td style='width: 130px'>{shipno}</td>" +
 		"<td style='width: 50px '>{status}</td>" +
-		"<td style='width: 100px'>{fee}</td>" +
-		"<td style='width: 100px'>{amount}</td>" +
+		"<td style='width: 80px'>{fee}</td>" +
+		"<td style='width: 80px'>{amount}</td>" +
 		"<td style='width: 80px '>{postno}</td>" +
-		"<td style='width: 180px'>{address1}</td>" +
-		"<td style='width: 250px'>{address2}</td>" +
-		"<td style='width: 250px'>{address3}</td>" +
-		"<td style='width: 150px'>{shipcontent}</td>" +
+		"<td style='width: 160px'>{address1}</td>" +
+		"<td style='width: 240px'>{address2}</td>" +
+		"<td style='width: 240px'>{address3}</td>" +
+		"<td style='width: 100px'>{name}</td>" +
+		"<td style='width: 120px'>{tel}</td>" +
+		"<td style='width: 120px'>{shipcontent}</td>" +
 		"<td style='width: 60px '>{shipcount}</td>" +
 		"<td style='width: 150px'>{shipdiv}</td>" +
 		"<td style='width: 200px'>{biko}</td>" +
