d821a660a81b977eb64beb6d247cebbfcd9a425b
brussliu <38742004+brussliu@users.noreply.github.com>
2022-05-20 22:35:19 +0900

    xxx
    sss

--- WEB-INF/efw/event/outputlabeldata.js
+++ WEB-INF/efw/event/outputlabeldata.js
@@ -8,11 +8,11 @@ outputlabeldata.fire=function(params){
 	
 	var ret = new Result();
 
-	var selectResult1 = db.select(
-		"LABEL",
-		"selectLabelData",
-		{"shop":"Smart-KM"}
-	).getArray();
+	// var selectResult1 = db.select(
+	// 	"LABEL",
+	// 	"selectLabelData",
+	// 	{"shop":"Smart-KM"}
+	// ).getArray();
 
 	var selectResult2 = db.select(
 		"LABEL",
@@ -25,19 +25,27 @@ outputlabeldata.fire=function(params){
 
 	var tempFilePathName = file.getTempFileName();
 
-	var sheet1 = 'Smart-KM';
-	for(var i = 0; i < selectResult1.length; i ++){
+	// var sheet1 = 'Smart-KM';
+	// for(var i = 0; i < selectResult1.length; i ++){
 
-		excel.setCell(sheet1, "A" + (i+2), selectResult1[i].productno);
-		excel.setCell(sheet1, "B" + (i+2), selectResult1[i].productname);
-		excel.setCell(sheet1, "C" + (i+2), selectResult1[i].color);
-		excel.setCell(sheet1, "D" + (i+2), selectResult1[i].size);
-		excel.setCell(sheet1, "E" + (i+2), selectResult1[i].labelno);
-		excel.setCell(sheet1, "F" + (i+2), selectResult1[i].asin);
-		excel.setCell(sheet1, "G" + (i+2), selectResult1[i].sku);
-		excel.setCell(sheet1, "H" + (i+2), selectResult1[i].sortno);
+	// 	excel.setCell(sheet1, "A" + (i+2), selectResult1[i].productno);
+	// 	excel.setCell(sheet1, "B" + (i+2), selectResult1[i].productname);
+	// 	excel.setCell(sheet1, "C" + (i+2), selectResult1[i].color);
+	// 	excel.setCell(sheet1, "D" + (i+2), selectResult1[i].size);
+	// 	excel.setCell(sheet1, "E" + (i+2), selectResult1[i].labelno);
+	// 	excel.setCell(sheet1, "F" + (i+2), selectResult1[i].asin);
+	// 	excel.setCell(sheet1, "G" + (i+2), selectResult1[i].sku);
+	// 	excel.setCell(sheet1, "H" + (i+2), selectResult1[i].sortno);
 
-	}
+	// }
+
+	var delResult = db.change(
+		"LABEL",
+		"deleteAllLabelInfo",
+		{
+		},
+		"jdbc/efw2"
+	);
 
 	var sheet2 = 'Smart-Bear';
 	for(var i = 0; i < selectResult2.length; i ++){
@@ -51,10 +59,28 @@ outputlabeldata.fire=function(params){
 		excel.setCell(sheet2, "G" + (i+2), selectResult2[i].sku);
 		excel.setCell(sheet2, "H" + (i+2), selectResult2[i].sortno);
 
+
+		
+		var insertResult = db.change(
+			"LABEL",
+			"insertProductLabel",
+			{
+				"col0":changeLabelValue(selectResult2[i].productno ),
+				"col1":changeLabelValue(selectResult2[i].productname),
+				"col2":changeLabelValue(selectResult2[i].color),
+				"col3":changeLabelValue(selectResult2[i].size),
+				"col4":changeLabelValue(selectResult2[i].labelno),
+				"col5":changeLabelValue(selectResult2[i].asin),
+				"col6":changeLabelValue(selectResult2[i].sku),
+				"col7":changeLabelValue(selectResult2[i].sortno)
+			},
+			"jdbc/efw2"
+		);
+
 	}
 
 
-	excel.setActiveSheet(sheet1).save(tempFilePathName);
+	excel.setActiveSheet(sheet2).save(tempFilePathName);
 
 	ret.attach(tempFilePathName)
 	.saveas("ラベル印刷用データ_" + (new Date()).format("yyyyMMdd")+".xlsx")
@@ -63,3 +89,11 @@ outputlabeldata.fire=function(params){
 	return ret;
 
 };
+
+function changeLabelValue(value){
+
+	if (value == null || value.length == 0){
+		return " ";
+	}
+
+}
