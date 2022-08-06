a576f88e769bba2e67dd1245f42083579071b08e
brussliu <38742004+brussliu@users.noreply.github.com>
2022-08-07 05:31:46 +0900

    a
    d

--- WEB-INF/efw/event/stock/updatestock.js
+++ WEB-INF/efw/event/stock/updatestock.js
@@ -31,38 +31,11 @@ updatestock.fire=function(params){
 				local : local,
 			}
 		);
-
-
-
-
-
-
-
-
-
-
 	}
 
-	// var selectResult = db.select(
-	// 	"STOCK",
-	// 	"selectstock",
-	// 	{
-	// 		shop : shopname,
-	// 		productdivstr1 : productdivstr1,
-	// 		productdivstr2 : productdivstr2,
-	// 		productdivstr3 : productdivstr3,
-	// 		productdivstr4 : productdivstr4,
-	// 		productdivstr5 : productdivstr5,
-	// 		productdivstr6 : productdivstr6,
-	// 		productdivstr7 : productdivstr7,
-	// 		productdivstr8 : productdivstr8,
-	// 		productno : productno,
-	// 		keyword : keyword
-	// 	}
-	// );
 
-	// var script = "$('#stocklist').show();changeColor();";
-	// ret.eval(script);
+	var script = "searchstock();";
+	ret.eval(script);
 
 	// 画面へ結果を返す
 	return ret;
